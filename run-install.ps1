ae2c790c75a8c8054f1c0033e47ed6f4359c5d4f
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-10-21 23:54:43 +0300

    Update run-install.ps1

--- run-install.ps1
+++ run-install.ps1
@@ -4,4 +4,4 @@ winget-install.ps1
 echo $env:USERPROFILE\.config\powershell\user_profile.ps1 > $PROFILE
 mkdir ~/.config
 cd ~/.config
-git pull https://github.com/lobanov4real/pwsh-user-profile.git
+git clone https://github.com/lobanov4real/pwsh-user-profile.git
