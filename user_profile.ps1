c065c7de0843b5c56b8a46b84fd651c7222dd37b
lobanov4real <lobanov4real@gmail.com>
2022-10-21 17:06:49 +0300

    add user profile for powershell

---
+++ user_profile.ps1
@@ -0,0 +1,45 @@
+oh-my-posh init pwsh --config "~\AppData\Local\Programs\oh-my-posh\themes\takuya.omp.json" | Invoke-Expression
+
+Set-PSReadLineOption -EditMode Emacs -PredictionSource History
+Set-PSReadLineOption -Colors @{
+	Command            = 'DarkCyan'
+	Number             = 'Gray'
+	Member             = 'DarkGray'
+	Operator           = 'Green'
+	Type               = 'Green'
+	Variable           = 'DarkGreen'
+	Parameter          = 'Green'
+	ContinuationPrompt = 'DarkGray'
+	Default            = 'Yellow'
+	Error              = 'red'
+	String             = 'DarkYellow'
+	}
+
+# Import-Module
+Import-Module Terminal-Icons
+Import-Module 7Zip4Powershell
+
+# Functions
+Function IndeedNovVPNOn {rasdial "Indeed-ID.VPN.General"}
+Function CleanTemp {sudo Remove-Item -Path $env:TEMP\* -Recurse -Force -ErrorAction SilentlyContinue}
+Function IndeedNovVPNOff {rasdial "Indeed-ID.VPN.General" /disconnect}
+Function shutdownoff {shutdown /p /t 1}
+Function shutdownreboot {shutdown /r /t 0}
+
+# Alias
+Set-Alias iion IndeedNovVPNOn
+Set-Alias iioff IndeedNovVPNOff
+Set-Alias cle clear
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias wi winget
+Set-Alias slack ~\AppData\Local\slack\slack.exe
+Set-Alias indeed '~\OneDrive\Documents\Indeed Identity.rdp'
+Set-Alias tlg '~\AppData\Roaming\Telegram Desktop\Telegram.exe'
+Set-Alias chrome 'C:\Program Files\Google\Chrome\Application\chrome.exe'
+Set-Alias zoom 'C:\Program Files\Zoom\bin\Zoom.exe'
+Set-Alias reboot shutdownreboot
+Set-Alias poweroff shutdownoff
+Set-Alias clean CleanTemp
