90db83d78f756c7b02b7124ea08173d6a7528f49
lobanov4real <lobanov4real@gmail.com>
2022-10-21 22:59:47 +0300

    add two files for install software

---
+++ module-install.ps1
@@ -0,0 +1,2 @@
+Install-Module -Name Terminal-Icons
+Install-Module -Name 7Zip4Powershell
---
+++ winget-install.ps1
@@ -0,0 +1,11 @@
+JanDeDobbeleer.OhMyPosh JanDeDobbeleer.OhMyPosh
+wi install -h --id Google.Chrome
+wi install -h --id Telegram.TelegramDesktop
+wi install -h --id Bitwarden.Bitwarden
+wi install -h --id Obsidian.Obsidian
+wi install -h --id Piriform.Speccy 
+wi install -h --id SlackTechnologies.Slack
+wi install -h --id Zoom.Zoom
+wi install -h --id Skillbrains.Lightshot
+wi install -h --id ProtonTechnologies.ProtonVPN
+wi install -h --id 7zip.7zip
