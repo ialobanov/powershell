427e332680e6361d6cde3d05d78b64d6099cf1b4
lobanov4real <lobanov4real@gmail.com>
2022-10-23 14:26:56 +0300

    add some code

--- run-install.ps1
+++ run-install.ps1
@@ -1,4 +1,4 @@
+Add-Content $PROFILE '.$env:USERPROFILE\.config\powershell\user_profile.ps1'
 ./module-install.ps1
 ./scoop-install.ps1
 ./winget-install.ps1
-Add-Content $PROFILE '.$env:USERPROFILE\.config\powershell\user_profile.ps1'
