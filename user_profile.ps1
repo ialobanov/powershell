6e2fda766580f374d162c7eb05e76f7069c86be9
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-10-24 10:39:02 +0300

    Update user_profile.ps1

--- user_profile.ps1
+++ user_profile.ps1
@@ -45,7 +45,7 @@ Set-Alias slack ~\AppData\Local\slack\slack.exe
 Set-Alias indeed '~\OneDrive\Documents\Indeed Identity.rdp'
 Set-Alias tlg '~\AppData\Roaming\Telegram Desktop\Telegram.exe'
 Set-Alias chrome 'C:\Program Files\Google\Chrome\Application\chrome.exe'
-Set-Alias zoom 'C:\Users\Ivan\AppData\Roaming\Zoom\bin\Zoom.exe'
+Set-Alias zoom '~\AppData\Roaming\Zoom\bin\Zoom.exe'
 Set-Alias reboot shutdownreboot
 Set-Alias poweroff shutdownoff
 Set-Alias clean CleanTemp
