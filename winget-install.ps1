b84d611d53d302d7d3943e34bb3036b27737be7d
lobanov4real <lobanov4real@gmail.com>
2022-10-28 20:01:34 +0300

    add to winget-install.ps1 some instalattion keys

--- winget-install.ps1
+++ winget-install.ps1
@@ -1,11 +1,12 @@
-wi install -h --id JanDeDobbeleer.OhMyPosh
-wi install -h --id Google.Chrome
-wi install -h --id Telegram.TelegramDesktop
-wi install -h --id Bitwarden.Bitwarden
-wi install -h --id Obsidian.Obsidian
-wi install -h --id Piriform.Speccy 
-wi install -h --id SlackTechnologies.Slack
-wi install -h --id Zoom.Zoom
-wi install -h --id Skillbrains.Lightshot
-wi install -h --id ProtonTechnologies.ProtonVPN
-wi install -h --id 7zip.7zip
+winget install --id JanDeDobbeleer.OhMyPosh -h -e --accept-source-agreements
+winget install --id Google.Chrome -h -e --accept-source-agreements
+winget install --id Telegram.TelegramDesktop -h -e --accept-source-agreements
+winget install --id Bitwarden.Bitwarden -h -e --accept-source-agreements
+winget install --id Obsidian.Obsidian -h -e --accept-source-agreements
+winget install --id Piriform.Speccy -h -e --accept-source-agreements
+winget install --id SlackTechnologies.Slack -h -e --accept-source-agreements
+winget install --id Zoom.Zoom -h -e --accept-source-agreements
+winget install --id Skillbrains.Lightshot -h -e --accept-source-agreements
+winget install -h --id ProtonTechnologies.ProtonVPN -h -e --accept-source-agreements
+winget install -h --id 7zip.7zip -h -e --accept-source-agreements
+winget install --id Google.Drive -h -e --accept-source-agreements
