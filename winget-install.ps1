e6d01720f8f91267ae2df9748aa1961e8033ac26
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-10-28 20:09:53 +0300

    Update winget-install.ps1

--- winget-install.ps1
+++ winget-install.ps1
@@ -7,6 +7,6 @@ winget install --id Piriform.Speccy -h -e --accept-source-agreements --accept-pa
 winget install --id SlackTechnologies.Slack -h -e --accept-source-agreements --accept-package-agreements
 winget install --id Zoom.Zoom -h -e --accept-source-agreements --accept-package-agreements
 winget install --id Skillbrains.Lightshot -h -e --accept-source-agreements --accept-package-agreements
-winget install -h --id ProtonTechnologies.ProtonVPN -h -e --accept-source-agreements --accept-package-agreements
-winget install -h --id 7zip.7zip -h -e --accept-source-agreements --accept-package-agreements
+winget install --id ProtonTechnologies.ProtonVPN -h -e --accept-source-agreements --accept-package-agreements
+winget install --id 7zip.7zip -h -e --accept-source-agreements --accept-package-agreements
 winget install --id Google.Drive -h -e --accept-source-agreements --accept-package-agreements
