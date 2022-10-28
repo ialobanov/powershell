f58bd9f53cd6010eaa00b8aaa6620f9461789bb3
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-10-28 20:09:37 +0300

    Update winget-install.ps1

--- winget-install.ps1
+++ winget-install.ps1
@@ -1,12 +1,12 @@
-winget install --id JanDeDobbeleer.OhMyPosh -h -e --accept-source-agreements
-winget install --id Google.Chrome -h -e --accept-source-agreements
-winget install --id Telegram.TelegramDesktop -h -e --accept-source-agreements
-winget install --id Bitwarden.Bitwarden -h -e --accept-source-agreements
-winget install --id Obsidian.Obsidian -h -e --accept-source-agreements
-winget install --id Piriform.Speccy -h -e --accept-source-agreements
-winget install --id SlackTechnologies.Slack -h -e --accept-source-agreements
-winget install --id Zoom.Zoom -h -e --accept-source-agreements
-winget install --id Skillbrains.Lightshot -h -e --accept-source-agreements
-winget install -h --id ProtonTechnologies.ProtonVPN -h -e --accept-source-agreements
-winget install -h --id 7zip.7zip -h -e --accept-source-agreements
-winget install --id Google.Drive -h -e --accept-source-agreements
+winget install --id JanDeDobbeleer.OhMyPosh -h -e --accept-source-agreements --accept-package-agreements
+winget install --id Google.Chrome -h -e --accept-source-agreements --accept-package-agreements 
+winget install --id Telegram.TelegramDesktop -h -e --accept-source-agreements --accept-package-agreements
+winget install --id Bitwarden.Bitwarden -h -e --accept-source-agreements --accept-package-agreements
+winget install --id Obsidian.Obsidian -h -e --accept-source-agreements --accept-package-agreements
+winget install --id Piriform.Speccy -h -e --accept-source-agreements --accept-package-agreements
+winget install --id SlackTechnologies.Slack -h -e --accept-source-agreements --accept-package-agreements
+winget install --id Zoom.Zoom -h -e --accept-source-agreements --accept-package-agreements
+winget install --id Skillbrains.Lightshot -h -e --accept-source-agreements --accept-package-agreements
+winget install -h --id ProtonTechnologies.ProtonVPN -h -e --accept-source-agreements --accept-package-agreements
+winget install -h --id 7zip.7zip -h -e --accept-source-agreements --accept-package-agreements
+winget install --id Google.Drive -h -e --accept-source-agreements --accept-package-agreements
