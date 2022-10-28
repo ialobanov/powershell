e00e84b6d4eeb86b206ffe9916114cb67e8317c9
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-10-28 20:24:26 +0300

    Update module-install.ps1

--- module-install.ps1
+++ module-install.ps1
@@ -1,5 +1,5 @@
 Install-Module -SkipPublisherCheck -AcceptLicense -Name Terminal-Icons -Scope CurrentUser
-Install-Module -SkipPublisherCheck -AcceptLicense -Name PSReadLine -Force -Scope CurrentUser
+Install-Module -SkipPublisherCheck -AcceptLicense -Name PSReadLine -Force -Scope CurrentUser -AllowPrerelease
 Install-Module -SkipPublisherCheck -AcceptLicense -Name 7Zip4Powershell -Scope CurrentUser
 Install-Module -SkipPublisherCheck -AcceptLicense -Name PSWindowsUpdate -Scope CurrentUser
 Install-Module -SkipPublisherCheck -AcceptLicense -Name VPNCredentialsHelper -Scope CurrentUser
