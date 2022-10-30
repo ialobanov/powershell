f1fac549972c77e544262bb0937bb15180bc5157
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-10-31 00:17:58 +0300

    Update module-install.ps1

--- module-install.ps1
+++ module-install.ps1
@@ -1,5 +1,5 @@
 Install-Module -SkipPublisherCheck -AcceptLicense -Name Terminal-Icons -Scope CurrentUser -Force
-Install-Module -SkipPublisherCheck -AcceptLicense -Name PSReadLine -Force -Scope CurrentUser -AllowPrerelease -Force
+Install-Module -SkipPublisherCheck -AcceptLicense -Name PSReadLine -Scope CurrentUser -AllowPrerelease -Force
 Install-Module -SkipPublisherCheck -AcceptLicense -Name 7Zip4Powershell -Scope CurrentUser -Force
 Install-Module -SkipPublisherCheck -AcceptLicense -Name PSWindowsUpdate -Scope CurrentUser -Force
 Install-Module -SkipPublisherCheck -AcceptLicense -Name VPNCredentialsHelper -Scope CurrentUser -Force
