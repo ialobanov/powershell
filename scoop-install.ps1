ed30d3224939c2ceb741d3b7e8612a3523338813
lobanov4real <lobanov4real@gmail.com>
2022-11-12 14:08:11 +0300

    added tree-sitter for scoop install

--- scoop-install.ps1
+++ scoop-install.ps1
@@ -1,3 +1,3 @@
 irm get.scoop.sh | iex
 scoop bucket add extras
-scoop install neovim curl sudo git webtorrent nodejs ripgrep wget fzf npm gcc luarocks  
+scoop install neovim curl sudo git webtorrent nodejs ripgrep wget fzf npm gcc luarocks tree-sitter  
