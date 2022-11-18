bdc8a62ec8da153a0962ffee8a32dcca93eab50f
lobanov4real <lobanov4real@gmail.com>
2022-11-18 18:45:31 +0300

    add innounp

--- scoop-install.ps1
+++ scoop-install.ps1
@@ -1,2 +1,2 @@
 scoop bucket add extras
-scoop install neovim curl sudo git webtorrent nodejs ripgrep wget fzf gcc luarocks tree-sitter python cmake gzip lua
+scoop install neovim curl sudo git webtorrent nodejs ripgrep wget fzf gcc luarocks tree-sitter python cmake gzip lua innounp
