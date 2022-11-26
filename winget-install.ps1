1d979798516e07762d408b3979db01b9bbfdbb1f
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-11-26 11:48:31 -0800

    Update winget-install.ps1

--- winget-install.ps1
+++ winget-install.ps1
@@ -2,11 +2,9 @@ winget install --id Google.Chrome -h -e --accept-source-agreements --accept-pack
 winget install --id Telegram.TelegramDesktop -h -e --accept-source-agreements --accept-package-agreements
 winget install --id Bitwarden.Bitwarden -h -e --accept-source-agreements --accept-package-agreements
 winget install --id Obsidian.Obsidian -h -e --accept-source-agreements --accept-package-agreements
-winget install --id Piriform.Speccy -h -e --accept-source-agreements --accept-package-agreements
 winget install --id SlackTechnologies.Slack -h -e --accept-source-agreements --accept-package-agreements
 winget install --id Zoom.Zoom -h -e --accept-source-agreements --accept-package-agreements
 winget install --id Skillbrains.Lightshot -h -e --accept-source-agreements --accept-package-agreements
 winget install --id ProtonTechnologies.ProtonVPN -h -e --accept-source-agreements --accept-package-agreements
 winget install --id 7zip.7zip -h -e --accept-source-agreements --accept-package-agreements
-winget install --id Google.Drive -h -e --accept-source-agreements --accept-package-agreements
 winget install --id ALCPU.CoreTemp -h -e --accept-source-agreements --accept-package-agreements
