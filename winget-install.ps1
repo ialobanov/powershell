2cc09e4fe0358596fedb53c948bf4b7b446ab760
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-11-27 12:31:29 -0800

    Update winget-install.ps1

--- winget-install.ps1
+++ winget-install.ps1
@@ -1,10 +1,10 @@
-winget install --id Google.Chrome -h -e --accept-source-agreements --accept-package-agreements --force  
-winget install --id Telegram.TelegramDesktop -h -e --accept-source-agreements --accept-package-agreements
-winget install --id Bitwarden.Bitwarden -h -e --accept-source-agreements --accept-package-agreements
-winget install --id Obsidian.Obsidian -h -e --accept-source-agreements --accept-package-agreements
-winget install --id SlackTechnologies.Slack -h -e --accept-source-agreements --accept-package-agreements
-winget install --id Zoom.Zoom -h -e --accept-source-agreements --accept-package-agreements
-winget install --id Skillbrains.Lightshot -h -e --accept-source-agreements --accept-package-agreements
+winget install --id Google.Chrome -h -e --accept-source-agreements --accept-package-agreements --force --source winget
+winget install --id Telegram.TelegramDesktop -h -e --accept-source-agreements --accept-package-agreements --force --source winget
+winget install --id Bitwarden.Bitwarden -h -e --accept-source-agreements --accept-package-agreements --force --source winget
+winget install --id Obsidian.Obsidian -h -e --accept-source-agreements --accept-package-agreements --force --source winget
+winget install --id SlackTechnologies.Slack -h -e --accept-source-agreements --accept-package-agreements --force --source winget
+winget install --id Zoom.Zoom -h -e --accept-source-agreements --accept-package-agreements --force --source winget
+winget install --id Skillbrains.Lightshot -h -e --accept-source-agreements --accept-package-agreements --force --source winget
 winget install --id ProtonTechnologies.ProtonVPN -h -e --accept-source-agreements --accept-package-agreements
-winget install --id 7zip.7zip -h -e --accept-source-agreements --accept-package-agreements
-winget install --id ALCPU.CoreTemp -h -e --accept-source-agreements --accept-package-agreements
+winget install --id 7zip.7zip -h -e --accept-source-agreements --accept-package-agreements --force --source winget
+winget install --id ALCPU.CoreTemp -h -e --accept-source-agreements --accept-package-agreements --force --source winget
