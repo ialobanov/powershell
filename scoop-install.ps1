6437650194ceb67bc803552a0d248726bfcd7217
Ivan Lobanov <110660329+lobanov4real@users.noreply.github.com>
2022-12-02 22:59:15 +0300

    Update scoop-install.ps1

--- scoop-install.ps1
+++ scoop-install.ps1
@@ -1,3 +1,3 @@
 scoop bucket add extras
-scoop install vcredist2022 neovim curl sudo git webtorrent nodejs ripgrep wget fzf gcc luarocks tree-sitter python cmake gzip lua innounp -s -k
+scoop install vcredist2022 neovim curl sudo git webtorrent nodejs ripgrep wget fzf gcc luarocks tree-sitter python cmake gzip lua innounp -s
 scoop uninstall vcredist2022
