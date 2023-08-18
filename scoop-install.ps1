218d108e3309fdc12c0043b5d8f175f1419c0417
lobanov4real <lobanov4real@gmail.com>
2023-08-18 16:45:35 +0300

    ref

--- scoop-install.ps1
+++ scoop-install.ps1
@@ -1,3 +1,3 @@
 scoop bucket add extras
-scoop install vcredist2022 neovim curl sudo webtorrent ripgrep wget fzf gcc lua luarocks tree-sitter gzip innounp -s
+scoop install vcredist2022 neovim curl sudo webtorrent ripgrep wget fzf gcc tree-sitter gzip innounp -s
 scoop uninstall vcredist2022
